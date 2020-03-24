ceef747082f639425a4b15a13de142a0c14b3e00
Kilian Kluge <kilian.kluge@gmail.com>
2020-03-24 16:35:00 +0100

    Refactor code #1

--- anchor_cnn.py
+++ anchor_cnn.py
@@ -1,77 +1,72 @@
+import spacy
+from alibi.utils.download import spacy_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import one_hot
+from alibi.explainers import AnchorText
+
 from spam_flat import load_data
+from spam_flat import stemSentence
 from spam_flat_cnn import load_model
 
+VOCAB_SIZE = 10000
+MAX_LENGTH = 350
 
-X_ws_train, X_ws_test, trainX, trainY, testX, testY = load_data(350,10000)
+class_names = ['Spam', 'Phishing']
+X_ws_train, X_ws_test, trainX, trainY, testX, testY = load_data(350, VOCAB_SIZE)
+
 model = load_model()
 
-
-import alibi
-from alibi.utils.download import spacy_model
-import spacy
 model_en = 'en_core_web_md'
 spacy_model(model=model_en)
 nlp = spacy.load(model_en)
-class_names = ['Spam','Phishing']
 
 
-from alibi.explainers import AnchorText
 # # we have to define a predict function that includes the transformation of the list of texts to a vector of numerical values
 def predict_cnn(text):
-    stem_texts = [stemSentence(x) for x in text]
-    encoded_texts = [one_hot(t,vocab_size) for t in stem_texts]
-    encoded_input = pad_sequences(encoded_texts,maxlen=max_length,padding="post",truncating="post")
-    return model.predict([encoded_input,encoded_input,encoded_input])
+    stem_texts = (stemSentence(x) for x in text)
+    encoded_texts = [one_hot(t, VOCAB_SIZE) for t in stem_texts]
+    encoded_input = pad_sequences(encoded_texts, maxlen=MAX_LENGTH, padding="post", truncating="post")
+    return model.predict([encoded_input, encoded_input, encoded_input])
 
 
-from spam_flat import stemSentence
-from keras.preprocessing.text import one_hot
-from keras.preprocessing.sequence import pad_sequences
-vocab_size = 10000
-max_length = 350
 explainer_anchor_cnn = AnchorText(nlp, predict_cnn)
 
-
-
-
-
 # for i in range(len(X_ws_test)):
-    # if int(round(predict_cnn([X_ws_test[i]])[0,1],0))==1 and testY[i][1]==1:
-        
-        # print('E-Mail:',i)
+# if int(round(predict_cnn([X_ws_test[i]])[0,1],0))==1 and testY[i][1]==1:
+
+# print('E-Mail:',i)
 # #test_email = X_ws_test[idx]
-        # pred = class_names[int(round(predict_cnn([X_ws_test[i]])[0,1],0))]
-        # alternative =  class_names[int(round(1 - predict_cnn([X_ws_test[i]])[0,0]))]
-        # print('Prediction: %s' % pred)
-        # print('True Class:', class_names[testY[i][1]])
-        # print('Probability(Phishing) =', predict_cnn([X_ws_test[i]])[0,1])
+# pred = class_names[int(round(predict_cnn([X_ws_test[i]])[0,1],0))]
+# alternative =  class_names[int(round(1 - predict_cnn([X_ws_test[i]])[0,0]))]
+# print('Prediction: %s' % pred)
+# print('True Class:', class_names[testY[i][1]])
+# print('Probability(Phishing) =', predict_cnn([X_ws_test[i]])[0,1])
 
 # Remove special signs 
-words = ['©','@','/',':','邀请您观看','的相册','年2月3日','提供者','查看相册','播放幻灯片','来自',
-         '的消息','如果您在阅读此电子邮件时出现问题，请将以下地址复制并粘贴到您的浏览器中',
-         '要分享您的照片或在朋友与您分享照片时收到通知，','请获取属于您自己的免费','%','网络相册帐户。','PCB Prototype FabricationPrinted circuit board jhh4 d ： PCB sample manufacture PCB sample manufacture 2015 ：PCB Prototype FabricationPrinted circuit board jhh4 d    PCB Prototype FabricationPrinted circuit board jhh4 d ： Dear SirMadam, Glad to know you from your great website, I’m proud to introduce you our high quality PCB products, Our advantage is –The most experienced PCB sample manufacture from CHINAQuick PCB Prototype'
- ,'customer’s requirementHigh'
- #,'quality with material Grade'
- , 'A FR-41 – 6'
-        ,' or manufacturing requirements description!We provide 10 discount','：']
+words = ['©', '@', '/', ':', '邀请您观看', '的相册', '年2月3日', '提供者', '查看相册', '播放幻灯片', '来自',
+         '的消息', '如果您在阅读此电子邮件时出现问题，请将以下地址复制并粘贴到您的浏览器中',
+         '要分享您的照片或在朋友与您分享照片时收到通知，', '请获取属于您自己的免费', '%', '网络相册帐户。',
+         'PCB Prototype FabricationPrinted circuit board jhh4 d ： PCB sample manufacture PCB sample manufacture 2015 ：PCB Prototype FabricationPrinted circuit board jhh4 d    PCB Prototype FabricationPrinted circuit board jhh4 d ： Dear SirMadam, Glad to know you from your great website, I’m proud to introduce you our high quality PCB products, Our advantage is –The most experienced PCB sample manufacture from CHINAQuick PCB Prototype'
+    , 'customer’s requirementHigh'
+         # ,'quality with material Grade'
+    , 'A FR-41 – 6'
+    , ' or manufacturing requirements description!We provide 10 discount', '：']
 
-X_ws_test1 = [] 
+X_ws_test1 = []
 for line in X_ws_test:
     for w in words:
         line = line.replace(w, '')
     X_ws_test1.append(line)
 
 for test_email in X_ws_test1:
-    if  testY[X_ws_test1.index(test_email)][1]== 1:
+    if testY[X_ws_test1.index(test_email)][1] == 1:
         print(test_email)
         print('Position Phishing E-Mail in Test Data', X_ws_test1.index(test_email))
-        explanation_anchor_cnn = explainer_anchor_cnn.explain(test_email, threshold=0.95,use_similarity_proba=False,
-                                use_unk=True, sample_proba=0.5,beam_size=5, tau = 0.7)
+        explanation_anchor_cnn = explainer_anchor_cnn.explain(test_email, threshold=0.95, use_similarity_proba=False,
+                                                              use_unk=True, sample_proba=0.5, beam_size=5, tau=0.7)
         print('Anchor: %s' % (' AND '.join(explanation_anchor_cnn['names'])))
         print('Precision: %.2f' % explanation_anchor_cnn['precision'])
         print(explanation_anchor_cnn)
-
-
 
 # print('Anchor: %s' % (' AND '.join(explanation_anchor_cnn['names'])))
 # print('Precision: %.2f' % explanation_anchor_cnn['precision'])
@@ -84,24 +79,24 @@ for test_email in X_ws_test1:
 # # In[15]:
 
 
-#print(explanation_anchor_cnn)
+# print(explanation_anchor_cnn)
 
 
 # # In[210]:
 
 
-#print(test_email)
-#X_ws_test_1 = X_ws_test[535:537]
-#filtered_numbers = [number for number in test_email if number != '©']
-#print(X_ws_test_1)
-#words = ['©']
+# print(test_email)
+# X_ws_test_1 = X_ws_test[535:537]
+# filtered_numbers = [number for number in test_email if number != '©']
+# print(X_ws_test_1)
+# words = ['©']
 ## # In[211]:
-#item1 = [] 
-#for line in X_ws_test_1:
+# item1 = []
+# for line in X_ws_test_1:
 #    for w in words:
 #        line = line.replace(w, '')
 #    item1.append(line)
-#print(item1)
+# print(item1)
 # print(explanation_anchor_cnn)
 
 
@@ -116,7 +111,3 @@ for test_email in X_ws_test1:
 
 
 # # In[ ]:
-
-
-
-
--- spam_flat.py
+++ spam_flat.py
@@ -5,62 +5,62 @@
 
 
 import os
-PATH = os.path.dirname(os.path.abspath("spam-flat3.csv"))
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
 
 def load_data(max_length, vocab_size):
     import pandas as pd
     from sklearn.model_selection import train_test_split
-    path = os.path.join(PATH, "spam-flat3.csv")
+    path = os.path.join(PATH, "data", "raw", "spam-flat3.csv")
     mydata = pd.read_csv(path)
-    #with_subject = mydata["subject"] + " " + mydata["bodyHtml"]
-    #with_subject = with_subject.fillna("Ignore")
+    # with_subject = mydata["subject"] + " " + mydata["bodyHtml"]
+    # with_subject = with_subject.fillna("Ignore")
     texts = mydata["bodyHtml"]
     texts = texts.fillna("Ignore")
-    #X_ws = with_subject.tolist()
+    # X_ws = with_subject.tolist()
     X_ws = texts.tolist()
-    mydata.loc[mydata['phishing'].astype(str) == 'False','phishing'] = 0
-    mydata.loc[mydata['phishing'].astype(str) == 'True','phishing'] = 1
+    mydata.loc[mydata['phishing'].astype(str) == 'False', 'phishing'] = 0
+    mydata.loc[mydata['phishing'].astype(str) == 'True', 'phishing'] = 1
     y = mydata['phishing'].astype(int).tolist()
-    X_ws_train,X_ws_test,y_train,y_test = train_test_split(X_ws,y,test_size=0.3,random_state=42)
-    trainX = preprocess_text(X_ws_train,max_length=max_length, vocab_size=vocab_size)
-    testX = preprocess_text(X_ws_test,max_length=max_length, vocab_size=vocab_size)
+    X_ws_train, X_ws_test, y_train, y_test = train_test_split(X_ws, y, test_size=0.3, random_state=42)
+    trainX = preprocess_text(X_ws_train, max_length=max_length, vocab_size=vocab_size)
+    testX = preprocess_text(X_ws_test, max_length=max_length, vocab_size=vocab_size)
     trainY = preprocess_label(y_train)
     testY = preprocess_label(y_test)
     return X_ws_train, X_ws_test, trainX, trainY, testX, testY
 
+
 def stemSentence(sentence):
     from nltk.tokenize import word_tokenize
     from nltk.stem import PorterStemmer
-    token_words=word_tokenize(sentence)
+    token_words = word_tokenize(sentence)
     token_words
-    stem_sentence=[]
+    stem_sentence = []
     for word in token_words:
         stem_sentence.append(PorterStemmer().stem(word))
         stem_sentence.append(" ")
         return "".join(stem_sentence)
 
-def preprocess_text(data_x, max_length = 350,vocab_size = 10000):
+
+def preprocess_text(data_x, max_length=350, vocab_size=10000):
     from keras.preprocessing.text import one_hot
     from keras.preprocessing.sequence import pad_sequences
     X_2 = [stemSentence(x) for x in data_x]
-    encoded_texts_ws = [one_hot(t,vocab_size) for t in X_2]
-    padded_texts_ws =pad_sequences(encoded_texts_ws,maxlen=max_length,padding="post",truncating="post")
+    encoded_texts_ws = [one_hot(t, vocab_size) for t in X_2]
+    padded_texts_ws = pad_sequences(encoded_texts_ws, maxlen=max_length, padding="post", truncating="post")
     return padded_texts_ws
+
 
 def preprocess_label(data_y):
     import numpy as np
-    y_1 =[]
+    y_1 = []
     for x in data_y:
-        if x==0:
-            y_1.append([1,0])
+        if x == 0:
+            y_1.append([1, 0])
         else:
-            y_1.append([0,1])
+            y_1.append([0, 1])
     y_1 = np.vstack(y_1)
     return y_1
 
-
 # In[ ]:
-
-
-
-
