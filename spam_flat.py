b34c9a16a2b13ccc8d6793eb13d7a5095180d1db
Kilian Kluge <kilian.kluge@gmail.com>
2020-03-24 17:09:58 +0100

    Only preprocess texts once

--- spam_flat.py
+++ spam_flat.py
@@ -3,15 +3,23 @@
 
 # In[1]:
 
-
 import os
+import pickle
+
+import numpy as np
+import pandas as pd
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import one_hot
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from sklearn.model_selection import train_test_split
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
+porter = PorterStemmer()
 
-def load_data(max_length, vocab_size):
-    import pandas as pd
-    from sklearn.model_selection import train_test_split
+
+def preprocess_data(max_length, vocab_size):
     path = os.path.join(PATH, "data", "raw", "spam-flat3.csv")
     mydata = pd.read_csv(path)
     # with_subject = mydata["subject"] + " " + mydata["bodyHtml"]
@@ -28,32 +36,35 @@ def load_data(max_length, vocab_size):
     testX = preprocess_text(X_ws_test, max_length=max_length, vocab_size=vocab_size)
     trainY = preprocess_label(y_train)
     testY = preprocess_label(y_test)
-    return X_ws_train, X_ws_test, trainX, trainY, testX, testY
+    data = (X_ws_train, X_ws_test, trainX, trainY, testX, testY)
+    with open(os.path.join(PATH, "data", "processed", f"spam-flat3_{max_length}_{vocab_size}.pickle"), "wb") as f:
+        pickle.dump(data, f)
+
+
+def load_data(max_length, vocab_size):
+    if not os.path.exists(os.path.join(PATH, "data", "processed", f"spam-flat3_{max_length}_{vocab_size}.pickle")):
+        print("Data not found, preprocessing data...")
+        preprocess_data(max_length, vocab_size)
+        print("Done preprocessing")
+
+    with open(os.path.join(PATH, "data", "processed", f"spam-flat3_{max_length}_{vocab_size}.pickle"), "rb") as f:
+        return pickle.load(f)
 
 
 def stemSentence(sentence):
-    from nltk.tokenize import word_tokenize
-    from nltk.stem import PorterStemmer
     token_words = word_tokenize(sentence)
-    token_words
-    stem_sentence = []
-    for word in token_words:
-        stem_sentence.append(PorterStemmer().stem(word))
-        stem_sentence.append(" ")
-        return "".join(stem_sentence)
+    stem_sentence = (porter.stem(word) for word in token_words)
+    return " ".join(stem_sentence)
 
 
 def preprocess_text(data_x, max_length=350, vocab_size=10000):
-    from keras.preprocessing.text import one_hot
-    from keras.preprocessing.sequence import pad_sequences
-    X_2 = [stemSentence(x) for x in data_x]
+    X_2 = (stemSentence(x) for x in data_x)
     encoded_texts_ws = [one_hot(t, vocab_size) for t in X_2]
     padded_texts_ws = pad_sequences(encoded_texts_ws, maxlen=max_length, padding="post", truncating="post")
     return padded_texts_ws
 
 
 def preprocess_label(data_y):
-    import numpy as np
     y_1 = []
     for x in data_y:
         if x == 0:
@@ -63,4 +74,8 @@ def preprocess_label(data_y):
     y_1 = np.vstack(y_1)
     return y_1
 
+
 # In[ ]:
+
+if __name__ == "__main__":
+    load_data(350, 10000)
