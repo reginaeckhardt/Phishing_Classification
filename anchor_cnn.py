c7af099451ddc0db503094f6de3b78c71f3bacbd
Kilian Kluge <kilian.kluge@gmail.com>
2020-03-24 17:29:17 +0100

    Fix model loading, clean up anchor_cnn.py

--- anchor_cnn.py
+++ anchor_cnn.py
@@ -1,21 +1,27 @@
+import string
+
 import spacy
+from alibi.explainers import AnchorText
 from alibi.utils.download import spacy_model
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import one_hot
-from alibi.explainers import AnchorText
 
 from spam_flat import load_data
 from spam_flat import stemSentence
-from spam_flat_cnn import load_model
+from models.spam_flat_cnn import load_model
 
 VOCAB_SIZE = 10000
 MAX_LENGTH = 350
 
 class_names = ['Spam', 'Phishing']
-X_ws_train, X_ws_test, trainX, trainY, testX, testY = load_data(350, VOCAB_SIZE)
 
-model = load_model()
+print("Loading data...")
+X_ws_train, X_ws_test, trainX, trainY, testX, testY = load_data(MAX_LENGTH, VOCAB_SIZE)
 
+print("Loading model...")
+model = load_model("spam_flat_cnn.h5")
+
+print("Loading NLP model...")
 model_en = 'en_core_web_md'
 spacy_model(model=model_en)
 nlp = spacy.load(model_en)
@@ -29,85 +35,22 @@ def predict_cnn(text):
     return model.predict([encoded_input, encoded_input, encoded_input])
 
 
+print("Initialize Anchor")
 explainer_anchor_cnn = AnchorText(nlp, predict_cnn)
 
-# for i in range(len(X_ws_test)):
-# if int(round(predict_cnn([X_ws_test[i]])[0,1],0))==1 and testY[i][1]==1:
+print("Remove all non-standard characters from e-mails")
+legal_characters = string.digits + string.ascii_letters + string.punctuation + ' '
+X_ws_test1 = ["".join(x if x in legal_characters else "" for x in line) for line in X_ws_test]
 
-# print('E-Mail:',i)
-# #test_email = X_ws_test[idx]
-# pred = class_names[int(round(predict_cnn([X_ws_test[i]])[0,1],0))]
-# alternative =  class_names[int(round(1 - predict_cnn([X_ws_test[i]])[0,0]))]
-# print('Prediction: %s' % pred)
-# print('True Class:', class_names[testY[i][1]])
-# print('Probability(Phishing) =', predict_cnn([X_ws_test[i]])[0,1])
+assert len(X_ws_test1) == len(X_ws_test)
 
-# Remove special signs 
-words = ['©', '@', '/', ':', '邀请您观看', '的相册', '年2月3日', '提供者', '查看相册', '播放幻灯片', '来自',
-         '的消息', '如果您在阅读此电子邮件时出现问题，请将以下地址复制并粘贴到您的浏览器中',
-         '要分享您的照片或在朋友与您分享照片时收到通知，', '请获取属于您自己的免费', '%', '网络相册帐户。',
-         'PCB Prototype FabricationPrinted circuit board jhh4 d ： PCB sample manufacture PCB sample manufacture 2015 ：PCB Prototype FabricationPrinted circuit board jhh4 d    PCB Prototype FabricationPrinted circuit board jhh4 d ： Dear SirMadam, Glad to know you from your great website, I’m proud to introduce you our high quality PCB products, Our advantage is –The most experienced PCB sample manufacture from CHINAQuick PCB Prototype'
-    , 'customer’s requirementHigh'
-         # ,'quality with material Grade'
-    , 'A FR-41 – 6'
-    , ' or manufacturing requirements description!We provide 10 discount', '：']
-
-X_ws_test1 = []
-for line in X_ws_test:
-    for w in words:
-        line = line.replace(w, '')
-    X_ws_test1.append(line)
-
-for test_email in X_ws_test1:
-    if testY[X_ws_test1.index(test_email)][1] == 1:
+for idx, test_email in enumerate(X_ws_test1[:50]):
+    if testY[idx][1] == 1:
         print(test_email)
         print('Position Phishing E-Mail in Test Data', X_ws_test1.index(test_email))
-        explanation_anchor_cnn = explainer_anchor_cnn.explain(test_email, threshold=0.95, use_similarity_proba=False,
-                                                              use_unk=True, sample_proba=0.5, beam_size=5, tau=0.7)
+        explanation_anchor_cnn = explainer_anchor_cnn.explain(test_email, desired_label=1, threshold=0.75,
+                                                              use_similarity_proba=False,
+                                                              use_unk=True, sample_proba=0.5, beam_size=1, tau=0.15)
         print('Anchor: %s' % (' AND '.join(explanation_anchor_cnn['names'])))
         print('Precision: %.2f' % explanation_anchor_cnn['precision'])
         print(explanation_anchor_cnn)
-
-# print('Anchor: %s' % (' AND '.join(explanation_anchor_cnn['names'])))
-# print('Precision: %.2f' % explanation_anchor_cnn['precision'])
-# print('\nExamples where anchor applies and model predicts %s:' % pred)
-# print('\n'.join([x[0] for x in explanation_anchor_cnn['raw']['examples'][-1]['covered_true']]))
-# print('\nExamples where anchor applies and model predicts %s:' % alternative)
-# print('\n'.join([x[0] for x in explanation_anchor_cnn['raw']['examples'][-1]['covered_false']]))
-
-
-# # In[15]:
-
-
-# print(explanation_anchor_cnn)
-
-
-# # In[210]:
-
-
-# print(test_email)
-# X_ws_test_1 = X_ws_test[535:537]
-# filtered_numbers = [number for number in test_email if number != '©']
-# print(X_ws_test_1)
-# words = ['©']
-## # In[211]:
-# item1 = []
-# for line in X_ws_test_1:
-#    for w in words:
-#        line = line.replace(w, '')
-#    item1.append(line)
-# print(item1)
-# print(explanation_anchor_cnn)
-
-
-# # In[1]:
-
-
-# pred_cnn = class_names[int(round(predict_cnn([test_email])[0,1]))]
-# alternative_cnn =  class_names[1 - int(round(predict_cnn([test_email])[0,1]))]
-# print('Probability(Phishing) in the CNN =', predict_cnn([test_email])[0,1])
-# print('Prediction CNN: %s' % pred_cnn)
-# print('True class: %s' % class_names[y_test[idx]])
-
-
-# # In[ ]:
---
+++ models/spam_flat_cnn.py
@@ -0,0 +1,41 @@
+import os
+
+import tensorflow as tf
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.metrics import Precision, Recall
+from numpy import array
+
+from cnn_model import build_model
+from spam_flat import load_data
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def save_model(model, fname):
+    model_path = os.path.join(PATH, "trained", fname)
+    model.save(model_path)
+
+
+def load_model(fname):
+    model_path = os.path.join(PATH, "trained", fname)
+    return tf.keras.models.load_model(model_path)
+
+
+def train_model(max_length=350, vocab_size=10000):
+    model = build_model(max_length=max_length, vocab_size=vocab_size)
+    X_ws_train, X_ws_test, trainX, trainY, testX, testY = load_data(max_length, vocab_size)
+    checkpoint = ModelCheckpoint('best_model.h5', monitor='val_precision', mode='max', verbose=1, save_best_only=True)
+    model.compile(loss='categorical_crossentropy', optimizer='adam',
+                  metrics=[Precision(name="precision", thresholds=0.7),
+                           Recall(name="recall")])
+    history = model.fit([trainX, trainX, trainX], array(trainY), validation_data=([testX, testX, testX], testY),
+                        epochs=50, verbose=2,
+                        callbacks=[EarlyStopping("val_precision", patience=10, restore_best_weights=True), checkpoint])
+    return model, history
+
+
+if __name__ == "__main__":
+    model, history = train_model()
+    print(model.summary())
+    save_model(model, "spam_flat_cnn.h5")
